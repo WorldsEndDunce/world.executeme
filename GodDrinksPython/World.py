a2798555c5985e64f726095858bf6265f0b88b62
Allison Tee <71671264+WorldsEndDunce@users.noreply.github.com>
2023-09-25 13:52:57 -0700

    Create World.py

---
+++ GodDrinksPython/World.py
@@ -0,0 +1,42 @@
+class World:
+    def __init__(self, value):
+        self.value = value
+
+    def addThing(self, thing):
+        pass
+
+    def startSimulation(self):
+        pass
+
+    def timeTravelForTwo(self, time, year, me, you):
+        pass
+
+    def unite(self, me, you):
+        pass
+
+    def lockThing(self, thing):
+        pass
+
+    def getGod(self):
+        pass
+
+    def procreate(self, me, you):
+        pass
+
+    def makeHigh(self, thing):
+        pass
+
+    def unlock(self, thing):
+        pass
+
+    def removeThing(self, thing):
+        pass
+
+    def isExecutableBy(self, thing):
+        pass
+
+    def execute(self, thing):
+        pass
+
+    def announce(self, message, language):
+        pass
